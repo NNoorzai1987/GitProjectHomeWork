afbc07138247702e0b619ca8f7d62a0487a647c2
Wellstudy1985 <Nnoorzai.001@gmail.com>
2022-08-27 20:08:26 -0700

    Here are the odd numbers code

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,18 @@
+package com.syntax;
+
+public class Main {
+    public static void main(String[] args) {
+       /* Task1:
+        1.Create a simple java project
+        2. Have a code in it that prints odd numbers from 1 to 100
+        3.create a local repository commit the code in your local repo
+        4. push the code to github in the main branch*/
+
+        for (int i = 1; i <=100 ; i+=2) {
+            System.out.print(i+" ");
+
+        }
+
+
+    }
+}
