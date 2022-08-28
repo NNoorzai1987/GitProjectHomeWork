f0d535fe0b5fc998823c16d98a1cc35db213e308
Wellstudy1985 <Nnoorzai.001@gmail.com>
2022-08-27 20:39:43 -0700

    This is Even Nambes code

---
+++ src/com/syntax/EvenNumbers.java
@@ -0,0 +1,19 @@
+package com.syntax;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+        /*Task2:
+        1.Clone the project that you created
+        2.create a new branch out of main
+        3.Modify the code for odd numbers to print even numbers from 1 to 100
+        4. commit the code in the created branch
+        5.Push your created branch to github*/
+
+        for (int i = 0; i <=100 ; i+=2) {
+            System.out.print(i+" ");
+
+        }
+
+    }
+
+}
